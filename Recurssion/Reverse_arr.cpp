ee158dffb32268bf89b044aa4eb8a1f58d792c1c
KumarRoushan9234 <roushan9234620012@gmail.com>
2025-02-22 04:18:40 +0530

    new

---
+++ Recurssion/Reverse_arr.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+void reverse_recr(int i, vector<int> nums)
+{
+}
+int main()
+{
+  int n;
+  cin >> n;
+  vector<int> nums(n);
+  for (int i = 0; i < n; i++)
+  {
+    cin >> nums[i];
+  }
+  cout << endl;
+  for (int i = 0; i < n; i++)
+  {
+    cout << nums[i] << " ";
+  }
+  return 0;
+}
